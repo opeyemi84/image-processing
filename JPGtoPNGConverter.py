ca4059fdb5f83d0413f43216edf07246769978fd
Opeyemi <opeadetola@gmail.com>
2023-11-16 15:03:03 -0500

    Upd image converter file

--- JPGtoPNGConverter.py
+++ JPGtoPNGConverter.py
@@ -1,9 +1,13 @@
 import sys 
 import os
-from PIL import image
+from PIL import Image
 
 # Step 1: grab the first and second argument 
 # (first, the image folder that has the jpg and secondly the new folder to store the converted files)
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+print(image_folder, output_folder)
 
 
 # Step 2: check if new folder exists, if not create it so as to store the new image files
